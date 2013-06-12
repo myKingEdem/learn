887084d062dc378dfd5ddbd6cf75b273b93936e8
Raymond Cudjoe <rkcudjoe@gmail.com>
2013-06-12 09:07:15 -0400

    complete iteration 3 and 4 of event manager

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,4 +1,9 @@
 require 'csv'
+require 'sunlight/congress'
+require 'erb'
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
 time = Time.now
 puts time.strftime('Event Manager - %m/%d/%Y, %I:%M%p')
 
@@ -6,9 +11,25 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0').slice(0..4)
 end
 
+def legislators_names(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
+
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exists?('output')
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, "w") { |file| file.puts form_letter  }
+end
+
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new(template_letter)
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-  puts "#{name} #{zipcode}"
+  legislators = legislators_names(zipcode)
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id,form_letter)
 end
